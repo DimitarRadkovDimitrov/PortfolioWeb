8837bb74572ed7d7dc08618e4494f9e135c9e767
Dimitar Dimitrov <dimitar.a.dimitrov@ericsson.com>
2018-11-30 12:34:38 -0500

    added general ericsson coop page and changed links

--- js/script.js
+++ js/script.js
@@ -1,1 +1,49 @@
-"use strict";function resizeDim(){var boxHeight=$('#about .content-box').outerHeight();var dim=$('#about-img-container');dim.attr('style','height: '+boxHeight+'px');}function switchActiveLink(){var navItemList=$('.navbar-nav').children().each(function(){let navItem=$(this);let link=navItem.find('.nav-link');if(link.hasClass('active')){navItem.addClass('active');}else{navItem.removeClass('active');}});}function switchToFloating(){let link=$('.navbar-nav').find('.nav-link.active').attr('href');if(link!=='#about'){$('header nav').addClass('floatingNav');}else{$('header nav').removeClass('floatingNav');}}$(document).ready(function(){const topOffset=$('header .navbar').outerHeight();resizeDim();$('#about').css('padding-top',topOffset);$('.section .sidebar').css('top',topOffset);$('body').scrollspy({target:'.navbar',offset:topOffset});switchActiveLink();switchToFloating();window.addEventListener('resize',resizeDim);$(document).on('click','a[href^="#"]',function(){event.preventDefault();$('html, body').animate({scrollTop:$($.attr(this,'href')).offset().top-topOffset+1},750);});$(window).on('activate.bs.scrollspy',switchActiveLink);$(window).on('activate.bs.scrollspy',switchToFloating);});
+"use strict";
+
+function resizeDim() {
+    var boxHeight = $('#about .content-box').outerHeight();
+    var dim = $('#about-img-container');
+    dim.attr('style', 'height: ' + boxHeight + 'px');
+}
+
+function switchActiveLink() {
+    var navItemList = $('.navbar-nav').children().each(function() {
+        let navItem = $(this);
+        let link = navItem.find('.nav-link');
+        if (link.hasClass('active')) {
+            navItem.addClass('active');
+        } else {
+            navItem.removeClass('active');
+        }
+    });
+}
+
+function switchToFloating() {
+    let link = $('.navbar-nav').find('.nav-link.active').attr('href');
+    if (link !== '#about') {
+        $('header nav').addClass('floatingNav');
+    } else {
+        $('header nav').removeClass('floatingNav');
+    }
+}
+$(document).ready(function() {
+    const topOffset = $('header .navbar').outerHeight();
+    resizeDim();
+    $('#about').css('padding-top', topOffset);
+    $('.section .sidebar').css('top', topOffset);
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: topOffset
+    });
+    switchActiveLink();
+    switchToFloating();
+    window.addEventListener('resize', resizeDim);
+    $(document).on('click', 'a[href^="#"]', function() {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - topOffset + 1
+        }, 750);
+    });
+    $(window).on('activate.bs.scrollspy', switchActiveLink);
+    $(window).on('activate.bs.scrollspy', switchToFloating);
+});
